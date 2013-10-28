78cbae26da1450d6ae256470e18f571bd18dfdbe
kingboyrang <aJia@bago.com.tw>
2013-10-28 13:39:58 +0800

    Initial Commit

---
+++ Wisdom/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Wisdom
+//
+//  Created by aJia on 2013/10/28.
+//  Copyright (c) 2013年 lz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Wisdom/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Wisdom
+//
+//  Created by aJia on 2013/10/28.
+//  Copyright (c) 2013年 lz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
